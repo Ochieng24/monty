1ad290dadcd9230789029a4f9fcb229eab6c2852
faithleo8 <leofaith8@gmail.com>
2023-08-24 16:39:50 +0300

    more files

---
+++ substitute.c
@@ -0,0 +1,47 @@
+ #include "monty.h"
+
+/**
+ * sub - Subtract two numbers
+ *
+ * @stack: Pointer to head of the stack
+ * @line_number: Line number in the file
+ *
+ * Return: Void
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+(void) stack; /* Unused parameter */
+(void) line_number; /* Unused parameter */
+}
+
+/**
+ * _sub - Subtract two numbers
+ *
+ * @head: Pointer to head of the stack
+ * @line_number: Line number in the file
+ *
+ * Return: EXIT_SUCCESS on success, else EXIT_FAILURE
+ */
+int _sub(stack_t **head, unsigned int line_number)
+{
+stack_t *temp = NULL;
+int element1 = 0, element2 = 2, exit_code = EXIT_SUCCESS;
+
+/* Check if there are at least two elements in the stack */
+if (*head == NULL || (*head)->next == NULL)
+{
+fprintf(stderr, "L%d: can't sub, stack too short\n", line_number); /* Print an error message */
+return (EXIT_FAILURE); /* Return failure code */
+}
+
+temp = *head; /* Store the current head in temp */
+element1 = (*head)->n; /* Get the value of the first element */
+element2 = (*head)->next->n; /* Get the value of the second element */
+element2 = element2 - element1; /* Calculate the difference of the two elements */
+(*head)->next->n = element2; /* Update the value of the second element */
+*head = (*head)->next; /* Move head to the next element */
+(*head)->prev = NULL; /* Update the previous pointer of the new head */
+free(temp); /* Free the memory of the previous head */
+return (exit_code); /* Return the exit code (EXIT_SUCCESS) */
+}
+
---
+++ swaps.c
@@ -0,0 +1,49 @@
+#include "monty.h"
+
+/**
+ * swap - Swaps the top two elements of the stack.
+ *
+ * @stack: Double pointer to the stack's head.
+ * @line_number: Line number of the current operation.
+ *
+ * Return: void
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+(void) stack; /* Unused parameter */
+(void) line_number; /* Unused parameter */
+}
+
+/**
+ * _swap - Swaps the top two elements of the stack.
+ *
+ * @head: Double pointer to the stack's head.
+ * @line_number: Line number of the current operation.
+ *
+ * Return: EXIT_SUCCESS if successful, otherwise EXIT_FAILURE.
+ */
+int _swap(stack_t **head, unsigned int line_number)
+{
+stack_t *node1 = NULL;
+stack_t *node2 = NULL;
+int exit_code = EXIT_SUCCESS;
+
+/* Check if there are at least two elements in the stack */
+if (*head == NULL || (*head)->next == NULL)
+{
+fprintf(stderr, "L%d: can't swap, stack too short\n", line_number); /* Print an error message */
+return (EXIT_FAILURE); /* Return failure code */
+}
+
+node1 = *head; /* Get the first node */
+node2 = (*head)->next; /* Get the second node */
+node1->next = node2->next; /* Update the next pointer of the first node */
+if (node1->next)
+node1->next->prev = node1; /* Update the previous pointer of the next node if it exists */
+node2->prev = NULL; /* Update the previous pointer of the second node */
+node2->next = node1; /* Update the next pointer of the second node */
+node1->prev = node2; /* Update the previous pointer of the first node */
+*head = node2; /* Update the head to the new second node */
+return (exit_code); /* Return the exit code (EXIT_SUCCESS) */
+}
+
