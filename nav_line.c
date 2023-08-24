466e9ce19b067eb2ddc1a25a7e922dc5a75d75c2
faithleo8 <leofaith8@gmail.com>
2023-08-24 06:01:21 -0700

    additional files

---
+++ nav_line.c
@@ -0,0 +1,52 @@
+#include "monty.h"
+
+/**
+ * parse_line - Parses a line of Monty bytecode and executes the corresponding instruction.
+ *
+ * @head: Double pointer to the stack's head.
+ * @line: Line of Monty bytecode.
+ * @line_number: Line number of the current bytecode instruction.
+ *
+ * Return: Exit code of the execution.
+ */
+int parse_line(stack_t **head, char *line, unsigned int line_number)
+{
+char *opcode = NULL, *operand = NULL;
+int exit_code = EXIT_SUCCESS, index = 0;
+
+instruction_t instructions[] = {
+{"push", push},
+{"pall", pall},
+{"pint", pint},
+{"pop", pop},
+{"swap", swap},
+{"add", add_two},
+{"sub", sub},
+{"div", div_two},
+{"mul", mul},
+{"nop", nop},
+{NULL, NULL}
+};
+opcode = strtok(line, DELIM); /* Get the opcode */
+operand = strtok(NULL, DELIM); /* Get the operand */
+if (opcode != NULL)
+{
+/* Search for the opcode in the list of instructions */
+for (; instructions[index].opcode != NULL; index++)
+{
+if (strcmp(instructions[index].opcode, opcode) == 0)
+{
+instructions[index].f(head, line_number); /* Execute the instruction function */
+exit_code = caller(head, opcode, operand, line_number); /* Call the caller function */
+break;
+}
+}
+/* If the opcode is not found in the list of instructions */
+if (instructions[index].opcode == NULL)
+{
+fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode); /* Print an error message */
+exit_code = EXIT_FAILURE; /* Update the exit code to failure */
+}
+}
+return (exit_code); /* Return the exit code */
+}
---
+++ palate.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+
+/**
+ * pall - Prints all the values on the stack, starting from the top.
+ *
+ * @stack: Double pointer to the stack's head.
+ * @line_number: Line number of the current operation.
+ *
+ * Return: void
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+(void) stack; /* Unused parameter */
+(void) line_number; /* Unused parameter */
+}
+
+/**
+ * _pall - Prints all the values on the stack, starting from the top.
+ *
+ * @head: Double pointer to the stack's head.
+ *
+ * Return: EXIT_SUCCESS if successful.
+ */
+int _pall(stack_t **head)
+{
+stack_t *current;
+current = *head;
+
+/* Iterate through the stack and print each element's value */
+while (current != NULL)
+{
+printf("%d\n", current->n); /* Print the value of the current element */
+current = current->next; /* Move to the next element */
+}
+return (EXIT_SUCCESS); /* Return the exit code (EXIT_SUCCESS) */
+}
